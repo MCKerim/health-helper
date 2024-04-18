d245c86a29f628057ea5b0d2cd2f1719a793512b
MCKerim <MCKerim@gmx.de>
2024-04-18 13:17:24 +0200

    Added firebase with firestore, auth and analytics

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUBvXjjPDy2ByKlT2S1_nZvhtZ28nxTXY",
+  authDomain: "health-helper-0.firebaseapp.com",
+  projectId: "health-helper-0",
+  storageBucket: "health-helper-0.appspot.com",
+  messagingSenderId: "1024000881087",
+  appId: "1:1024000881087:web:1eda1c68457386e21df10a",
+  measurementId: "G-EF3NW2WPX2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app)
+export const auth = getAuth(app)
