29d30b6a0571f24569aeeaa485cca4a4b955b582
MCKerim <MCKerim@gmx.de>
2024-04-18 13:25:07 +0200

    Added react router, added login and signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/signup.tsx/SignUp";
+import SignIn from "./pages/login/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Hello World</h1>} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/login/SignIn.tsx
@@ -0,0 +1,9 @@
+
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
