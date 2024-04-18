0076a5cc72d4f9877af47114fce65b170c908a58
MCKerim <MCKerim@gmx.de>
2024-04-18 13:28:20 +0200

    cleanup, added chat page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup.tsx/SignUp";
 import SignIn from "./pages/login/SignIn";
+import Chat from "./pages/chat/Chat";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Hello World</h1>} />
+      <Route path="/" element={<Chat />} />
+      <Route path="/chat" element={<Chat />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
     </Routes>
