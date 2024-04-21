28cb28f7b075a5fb5b7df6efb39617382cd75823
Aldo Costa <aldo.costa@web.de>
2024-04-21 21:10:59 +0200

    Basic styling to MessageBox

--- src/components/atoms/messageBox/MessageBox.tsx
+++ src/components/atoms/messageBox/MessageBox.tsx
@@ -1,14 +1,20 @@
 import "./MessageBox.css";
 
 export type Message = {
-  sender: "user" | "bot"
-  message: string
-}
+  sender: "user" | "bot";
+  message: string;
+};
 
 export default function MessageBox({ sender, message }: Message) {
   return (
-    <div className={"MessageBox " + (sender === "user" ? "MessageBoxUser" : "MessageBoxBot")}>
-      <p className="MessageSender">{sender}:</p>
+    <div
+      className={
+        "MessageBox " + (sender === "user" ? "MessageBoxUser" : "MessageBoxBot")
+      }
+    >
+      <p className="MessageSender">
+        {sender === "user" ? "Du" : "Health Helper"}:
+      </p>
       <p className="MessageText">{message}</p>
     </div>
   );
