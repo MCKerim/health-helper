57dd41a2dd2e2a070891b6f9cdf4da8fc42d58bf
Aldo Costa <aldo.costa@web.de>
2024-05-03 18:38:34 +0200

    Added EventListener for toggling the menu drawer on an outside click & small Message Box styling adjustment

--- src/components/molecules/drawerMenu/DrawerMenu.tsx
+++ src/components/molecules/drawerMenu/DrawerMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DrawerMenu from "../../atoms/drawerMenuContent/DrawerMenuContent";
 import HamburgerMenu from "../../atoms/drawerHamburgerIcon/DrawerHamburgerIcon";
 
@@ -8,8 +8,31 @@ interface AppProps {
 }
 
 const App: React.FC<AppProps> = ({ isOpen, toggleDrawer }) => {
+  const node = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Function to handle click events on document
+    const handleClickOutside = (event: MouseEvent) => {
+      // Check if the click is outside the node
+      if (node.current && !node.current.contains(event.target as Node)) {
+        // If drawer is open and click is outside, toggle the drawer
+        if (isOpen) {
+          toggleDrawer();
+        }
+      }
+    };
+
+    // Add event listener to document
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Clean up event listener from the document
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, toggleDrawer]);
+
   return (
-    <div>
+    <div ref={node}>
       <HamburgerMenu isOpen={isOpen} toggleDrawer={toggleDrawer} />
       <DrawerMenu isOpen={isOpen} />
     </div>
