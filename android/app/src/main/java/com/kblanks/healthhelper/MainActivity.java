7fd4aae8cbaceb05597be410c537f392cc96bf34
Aldo Costa <aldo.costa@web.de>
2024-05-20 15:42:16 +0200

    Fixed App Overscroll

--- android/app/src/main/java/com/kblanks/healthhelper/MainActivity.java
+++ android/app/src/main/java/com/kblanks/healthhelper/MainActivity.java
@@ -1,5 +1,17 @@
 package com.kblanks.healthhelper;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.view.View;
+import android.webkit.WebView;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView v = getBridge().getWebView();
+        v.setOverScrollMode(View.OVER_SCROLL_NEVER);
+    }
+}
