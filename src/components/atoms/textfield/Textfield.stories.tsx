e731099b1def2900acf9dd8361423d830a5df057
MCKerim <MCKerim@gmx.de>
2024-06-05 15:29:17 +0200

    Wrote stories for Textfield

--- src/components/atoms/textfield/Textfield.stories.tsx
+++ src/components/atoms/textfield/Textfield.stories.tsx
@@ -14,11 +14,38 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Text: Story = {
   args: {
     label: "Email",
     placeholder: "example@gmail.com",
     value: "",
+    type: "email",
+  },
+};
+
+export const Password: Story = {
+  args: {
+    label: "Password",
+    placeholder: "********",
+    value: "",
+    type: "password",
+  },
+};
+
+export const Contained: Story = {
+  args: {
+    placeholder: "Enter Text...",
+    value: "",
     type: "text",
+    contained: true,
+  },
+};
+
+export const ContainedPassword: Story = {
+  args: {
+    placeholder: "Password",
+    value: "",
+    type: "password",
+    contained: true,
   },
 };
