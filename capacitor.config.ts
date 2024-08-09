983d32af80aaec879e973947d096f71f76af540f
MCKerim <MCKerim@gmx.de>
2024-08-09 22:00:21 +0200

    App store Release 1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.kblanks.healthhelper',
-  appName: 'health-helper',
+  appName: 'Health Helper',
   webDir: 'build'
 };
 
